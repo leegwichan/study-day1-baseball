b8399e75c4b4c14b946ef48b9b5d8fdb1aa29cc2
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-11-27 13:27:37 +0900

    fix(Number) : 팩토리 패턴을 사용하여 값을 조정하고 싶었으나, 임의적으로 Number를 다른 곳에서도 생성 할 수 있어서 조금 더 연구 예정

--- src/main/java/baseball/number/Number.java
+++ src/main/java/baseball/number/Number.java
@@ -2,20 +2,27 @@ package baseball.number;
 
 public class Number {
 
+    private static final char MIN_CHAR = '9';
+    private static final char MAX_CHAR = '9';
+
     private final char number;
 
-    public Number(char minChar, char maxChar, char number) {
-        validateNumberInRange(minChar, maxChar, number);
+    public Number(char number) {
+        validateNumberInRange(number);
         this.number = number;
     }
 
-    private void validateNumberInRange(char minChar, char maxChar, char number) {
-        if (number < minChar || number > maxChar) {
+    private void validateNumberInRange(char number) {
+        if (number < MIN_CHAR || number > MAX_CHAR) {
             throw new IllegalArgumentException();
         }
     }
 
     public char getNumber() {
         return number;
+    }
+
+    public boolean isEqual(Number number) {
+        return this.number == number.getNumber();
     }
 }
